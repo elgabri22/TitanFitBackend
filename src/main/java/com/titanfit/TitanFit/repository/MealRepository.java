97196bf0b47bb4ad737124efc475287eb17dc3b6
Gabriel <gabriel.denia@petroprix.com>
2025-06-10 08:57:59 +0200

    Proyecto terminado

--- src/main/java/com/titanfit/TitanFit/repository/MealRepository.java
+++ src/main/java/com/titanfit/TitanFit/repository/MealRepository.java
@@ -2,13 +2,10 @@ package com.titanfit.TitanFit.repository;
 
 import com.titanfit.TitanFit.model.Meal;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface MealRepository extends MongoRepository<Meal, String> {
     List<Meal> findMealsByfecha (String fecha);
-    @Query("SELECT m FROM Meal m WHERE m.fecha BETWEEN :startDate AND :endDate")
-    List<Meal> findByfechaBetween(@Param("startDate") String startDate, @Param("endDate") String endDate);
+    List<Meal> findByfechaBetween(String startDate, String endDate);
 }
